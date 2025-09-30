f639b1814034d152b0e3e5e4f3dd1036bb514216
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-30 20:37:53 +0300

    Задание 5

---
+++ Задание 5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Задание_5
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Ждём данные... Осталось 2 секунды.");
+
+            int result = await GetDataAsync();
+
+            Console.WriteLine("Данные получены!");
+            Console.WriteLine($"Результат: {result}");
+        }
+        static async Task<int> GetDataAsync()
+        {
+            await Task.Delay(2000);
+            return 0;
+        }
+    }
+}
