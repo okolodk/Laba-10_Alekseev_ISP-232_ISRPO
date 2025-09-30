ed04498db11da98ff2bf8132453453260384779c
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-30 19:26:19 +0300

    Задание 3

---
+++ Задание_3/Interface1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3
+{
+    internal interface IPrintable
+    {
+        void PrintInfo();
+    }
+    public class Book : IPrintable
+    {
+        private static Random random = new Random();
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Книге: {random.Next(1, 100)} лет.");
+        }
+    }
+
+    public class Magazine : IPrintable
+    {
+        private static Random random = new Random();
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Журналу: {random.Next(1, 20)} лет.");
+        }
+    }
+}
---
+++ Задание_3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Задание_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IPrintable> printables = new List<IPrintable>
+            {
+                new Book(),
+                new Magazine(),
+                new Book(),
+                new Magazine(),
+            };
+
+            foreach (var printable in printables)
+            {
+                printable.PrintInfo();
+            }
+        }
+    }
+}
