c17b42f9404e86799ad367294653f565a116eec5
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-30 18:51:23 +0300

    Задание 2

---
+++ Задание_2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Задание_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> digits = new List<int>();
+            for (int i = 0; i < 20; i ++)
+            {
+                digits.Add(i + 1);
+            }
+            List<int> evenDigits = digits.FindAll(c => c % 2 == 0);
+
+            List<int> multipleThreeDigits = digits.FindAll(c => c % 3 == 0);
+
+            Console.WriteLine("Чётные числа: " + string.Join(", ", evenDigits));
+            Console.WriteLine();
+            Console.WriteLine("Числа, кратные 3: " + string.Join(", ", multipleThreeDigits));
+        }
+    }
+}
